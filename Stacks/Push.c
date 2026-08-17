33e5790b6728e7234136846234b05f4078738a8d
Achala Eshan <achalaeshan20@gmail.com>
2026-08-17 20:43:57 +0530

    push method is createdd😍

---
+++ Stacks/Push.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define SIZE 5
+int stack[SIZE];
+int top=-1;
+
+void push(int value){
+    if(top==SIZE -1){
+        printf("Stack is full");
+    }else{
+        top++;
+        stack[top]=value;
+    }
+}
+
+int main(){
+    push(50);
+}
